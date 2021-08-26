4adae513a53e7680fe5b7c399899517a5f3cec22
iamasadullah <iiasadkhann@gmail.com>
2021-08-26 09:54:02 +0500

    image removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'demoProject';
+
+
+  ngOnInit() {
+    const smile = String.fromCodePoint(0x1F920);
+    // const str = `Smile ${smile} you're on TV.`;
+    alert(`Welcome to the project! ${smile}`);
+  }
 }
